c6555014fc1a49772b4c662f0ea3965a64bd7c5b
Luke Edwards-Sparsholtt <87570488+DoitlikeJordan23@users.noreply.github.com>
2023-09-08 19:28:06 +0100

    Update script.js

---
+++ NatureUI/HTML/js/script.js
@@ -0,0 +1,14 @@
+// Wait for the DOM to be ready before executing JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+  // Your JavaScript code goes here
+
+  // Example: Adding a click event listener to a button
+  const myButton = document.getElementById("myButton");
+  myButton.addEventListener("click", function () {
+    // Code to run when the button is clicked
+    alert("Button clicked!");
+  });
+
+  // You can add more code to manipulate your website here
+
+});
